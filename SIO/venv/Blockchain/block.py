b0c7594bb12512b24eabd9c62bbdbde5e24ae6a8
jpribeiro7 <josepribeiro@ua.pt>
2018-11-28 19:35:39 +0000

    blockchain skeleton

---
+++ SIO/venv/Blockchain/block.py
@@ -0,0 +1,25 @@
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+import base64
+
+class Block:
+    def __init__(self,previous_hash, amount, description, pubkey, cc):
+        self.previous_hash = previous_hash
+        self.amount = amount
+        self.description = description
+        self.pubkey = pubkey
+        self.cc = cc
+        self.hash = self.build_hash()
+
+
+    def build_hash(self):
+        digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+        data = str(self.previous_hash)+str(self.amount)+self.description+str(self.pubkey)+str(self.cc)
+
+        digest.update(data.encode('utf-8'))
+
+        return base64.encodebytes(digest.finalize())
+
+    def getBlockHash(self):
+        return self.hash
---
+++ SIO/venv/Blockchain/blockchain.py
@@ -0,0 +1,49 @@
+from Blockchain.block import Block
+
+
+
+class BlockChain:
+    count = 0
+    def __init__(self, max_blocks):
+        self.id = BlockChain.count
+        BlockChain.count+=1
+        self.chain = []
+        self.current_block= 0
+        self.max_blocks =max_blocks
+
+
+
+    def add(self, amount, description, cc, pubkey):
+
+        if self.chain == []:
+            b = Block(0, amount, description, cc, pubkey)
+            self.chain.append(b)
+        elif self.current_block +1  < self.max_blocks:
+            b = Block(self.chain[self.current_block].getBlockHash(), amount, description, cc, pubkey)
+            self.chain.append(b)
+            self.current_block += 1
+
+
+    def print(self):
+        for block in self.chain:
+            print(block.getBlockHash())
+
+
+some = BlockChain(max_blocks = 3)
+some.add(20, "aa",12,12)
+some.add(20, "aa",12,12)
+some.add(20, "aa",12,12)
+some.add(20, "aa",12,12)
+some.print()
+
+
+
+
+
+
+
+
+
+
+
+
