21ba0bc5e4699591ed20359b0653446758a4e468
Joao Serpa <joaoserpa1997@outlook.com>
2018-12-05 18:09:24 +0000

    Added SessionKey construction
    Session key is now created.

--- SIO/venv/AuctionManager/RunAuctionManager.py
+++ SIO/venv/AuctionManager/RunAuctionManager.py
@@ -1,8 +1,19 @@
 import socket
 import sys
 import base64
-from venv.APP.App import *
 
+from venv.APP.App import *
+import json
+import codecs
+import pickle
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.serialization import load_pem_parameters
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import serialization
+
+#Function to create a auction
 def createAuction():
     message = "Create Auction"
     message = base64.b64encode(message.encode("utf-8"))
@@ -25,9 +36,29 @@ def createAuction():
         print('closing socket')
         sock.close()
 
+#Function to create a session key
+def createSessionKey(message_json, address):
 
+    #decode the data
+    parameters = pickle.loads(codecs.decode(message_json["data"].encode(), "base64"))
+    par = load_pem_parameters(parameters, backend=default_backend())
 
+    #Generate our public/private key
+    private_key = par.generate_private_key()
+    public_key = private_key.public_key()
 
+    #Get the public key from the user
+    peer_public_key_bytes = message_json["pk"].encode()
+    peer_public_key = serialization.load_pem_public_key(peer_public_key_bytes,default_backend())
+    shared_key = private_key.exchange(peer_public_key)
+    derived_key = HKDF(algorithm=hashes.SHA256(),length=32,salt=None,info=b'handshake data',
+                       backend=default_backend()).derive(shared_key)
+
+    #Now send our public key to the client
+    message = public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                            format=serialization.PublicFormat.SubjectPublicKeyInfo).decode('utf-8')
+
+    sent = sock.sendto(base64.b64encode(message.encode('utf-8')), address)
 
 
 
@@ -43,11 +74,22 @@ while True:
     print('\nwaiting to receive message')
     data, address = sock.recvfrom(4096)
     print(base64.b64decode(data))
+
+    decoded_data = base64.b64decode(data)
+
     print('received {} bytes from {}'.format(
         len(data), address))
-    print(data)
-    if(base64.b64decode(data).decode('utf-8') == "createAuction"):
-        createAuction();
+
+    message_json = json.loads(decoded_data,strict=False)
+
+
+    if message_json["type"] == "createAuction":
+        createAuction()
+    elif message_json["type"] == "session":
+        createSessionKey(message_json,address)
+
+
+
     if data:
         sent = sock.sendto(data, address)
         print('sent {} bytes back to {}'.format(
--- SIO/venv/ClientSide/RunClient.py
+++ SIO/venv/ClientSide/RunClient.py
@@ -1,9 +1,20 @@
+from locale import format
+
 from venv.ClientSide.ClientActions import *
 from venv.ClientSide.Client import *
 from venv.APP.App import *
 import socket
 import base64
 import sys
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.serialization import Encoding
+from cryptography.hazmat.primitives.serialization import ParameterFormat
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+import codecs
+import pickle
 
 #This class is responsible for the comunication between the system and the client
 
@@ -32,8 +43,55 @@ def switch(op):
     return (msg, address)
 
 
+#initializes the session key
+#Crying in python
+def sessionKeyInit():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    #Our parameters
+    parameters = dh.generate_parameters(generator=5, key_size=512,backend = default_backend())
+
+    #Our private key and public key
+    private_key = parameters.generate_private_key()
+    public_key = private_key.public_key()
+
+    #needs the server parameters, send our parameters, for now our parameters are the ones to be done
+    message = codecs.encode(pickle.dumps(parameters.parameter_bytes(encoding=Encoding.PEM,format=ParameterFormat.PKCS3)),"base64" ).decode()
+
+    #message construction
+    json_message = "{ " + "\n"
+    json_message += "\"type\" : \"session\","+ "\n"
+    json_message += "\"data\" : \"" + message + "\", \n"
+    json_message += "\"pk\" : \"" + public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                            format=serialization.PublicFormat.SubjectPublicKeyInfo).decode('utf-8') + "\""
+    json_message += "}"
+
+    print(json_message)
+    try:
+        #send response
+        sent = sock.sendto(base64.b64encode(json_message.encode('utf-8')), AM_ADDRESS)
+
+        # Receive response
+        data, server = sock.recvfrom(4096)
+        #derivate the key
+        peer_public_key_bytes = base64.b64decode(data)
+        peer_public_key = serialization.load_pem_public_key(peer_public_key_bytes, default_backend())
+        shared_key = private_key.exchange(peer_public_key)
+
+        derived_key = HKDF(algorithm=hashes.SHA256(), length=32, salt=None, info=b'handshake data',
+                           backend=default_backend()).derive(shared_key)
+
+    finally:
+        sock.close()
+
+
+
 def menu():
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
     op = -1
+    sessionKeyInit()
     while(op != 6):
         print("1 - Login")
         print("2 - Create an auction")
@@ -48,10 +106,6 @@ def menu():
         message, server_address = switch(int(op))
 
         message = base64.b64encode(message.encode("utf-8"))
-
-        # Create a UDP socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 
         try:
             # Send data
